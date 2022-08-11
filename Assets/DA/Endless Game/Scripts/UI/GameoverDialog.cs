c2a2b88ad830c1eeae424f14938c583baa5203c1
Duyy285 <106329709+duyanh285@users.noreply.github.com>
2022-08-11 10:46:57 +0700

    Gameover Dialog script handie
    Gameover Dialog script handie

--- Assets/DA/Endless Game/Scripts/UI/GameoverDialog.cs
+++ Assets/DA/Endless Game/Scripts/UI/GameoverDialog.cs
@@ -1,11 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
 
 namespace DA.Endless
 {
     public class GameoverDialog : Dialog
     {
+        public Text socreTxt;
+        public Text bestScoreTxt;
+
+        public override void Show(bool isShow)
+        {
+            base.Show(isShow);
+
+            if (socreTxt)
+                socreTxt.text = GameManager.Ins.Score.ToString();
+
+            if (Pref.hasBestScore)
+            {
+                if (bestScoreTxt)
+                    bestScoreTxt.text = $"NEW BEST:{Pref.bestScore}";
+            }
+            else
+            {
+                if (bestScoreTxt)
+                    bestScoreTxt.text = $"TOP SCORE:{Pref.bestScore}";
+            }
+        }
+
+        public void BackHome()
+        {
+            Close();
+            SceneManager.LoadScene(GameScene.MainMenu.ToString());
+        }
+
+        public void Replay()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 
     }
 }
