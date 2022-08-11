bb725124069d784e630c82873e49d2c91170ab0d
Duyy285 <106329709+duyanh285@users.noreply.github.com>
2022-08-11 11:18:01 +0700

    Character animations fixe
    Character animations fixe

--- Assets/DA/Endless Game/Scripts/UI/LevelDialog.cs
+++ Assets/DA/Endless Game/Scripts/UI/LevelDialog.cs
@@ -31,7 +31,7 @@ namespace DA.Endless
             if (bestScoreTxt)
                 bestScoreTxt.text = $"TOP SCORE: {Pref.bestScore}";
 
-           Pref.bestScore = 1500;
+           Pref.bestScore = 3000;
 
             UpdateUI();
         }
